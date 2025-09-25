5ee368e4e2bc6bbecbb9acab646815da14465632
bahrom04 <magdiyevbahrom@gmail.com>
2025-09-25 16:19:32 +0500

    chore: update install.sh for Mac and Debiloid

--- install.sh
+++ install.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
-# This script installs the Uzbek Latin Hunspell dictionary on a Debian-based system.
+# This script installs the Uzbek Latin Hunspell dictionary on different platforms.
+# Detect the platform
 
-cp ./uz_Latn.dic /usr/share/hunspell/uz_UZ.dic
-cp ./uz_Latn.aff /usr/share/hunspell/uz_UZ.aff
+OS="$(uname)"
 
+case "$OS" in 
+  'Linux')
+    # Debiloid only
+    echo "🔧 Copying to /usr/share/hunspell (requires sudo)"
+    sudo cp ./uz_UZ.dic /usr/share/hunspell/uz_UZ.dic
+    sudo cp ./uz_UZ.aff /usr/share/hunspell/uz_UZ.aff
+    sudo cp ./uz_UZ_Cyrl.dic /usr/share/hunspell/uz_UZ_Cyrl.dic
+    sudo cp ./uz_UZ_Cyrl.aff /usr/share/hunspell/uz_UZ_Cyrl.aff
+    echo "✅ Installed on Linux"
+    ;;
+
+  'Darwin') 
+    echo "🔧 Copying to ~/Library/Spelling"
+    mkdir -p ~/Library/Spelling
+    cp ./uz_UZ.dic ~/Library/Spelling/uz_UZ.dic
+    cp ./uz_UZ.aff ~/Library/Spelling/uz_UZ.aff
+    cp ./uz_UZ_Cyrl.dic ~/Library/Spelling/uz_UZ_Cyrl.dic
+    cp ./uz_UZ_Cyrl.aff ~/Library/Spelling/uz_UZ_Cyrl.aff
+    echo "✅ Installed on macOS"
+    ;;
+
+  'FreeBSD')
+    echo "Detected FreeBSD (not supported yet)"
+    ;;
+
+  'WindowsNT' | 'MINGW'* | 'CYGWIN'*)
+    echo "Detected Windows (not supported by this script)"
+    ;;
+
+  *)
+    echo "Unknown OS: $OS"
+    ;;
+
+esac
