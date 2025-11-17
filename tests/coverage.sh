8a1c3b2786da50568913c1481b50b643bfa96ec8
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2025-11-18 00:29:03 +0900

    Add unique coverage test

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -8,4 +8,10 @@ COUNT=$(hunspell -d $DICTIONARY -l $CORPUS | wc -l)
 
 RESULT=$(echo "scale=2; ($TOTAL - $COUNT)*100/$TOTAL" | bc)
 
+UNIQUE_TOTAL=$(tr -s '[:space:]' '\n' < $CORPUS | sort | uniq | wc -w --total=only)
+UNIQUE_COUNT=$(hunspell -d $DICTIONARY -l $CORPUS | sort | uniq | wc -l)
+
+UNIQUE_RESULT=$(echo "scale=2; ($UNIQUE_TOTAL - $UNIQUE_COUNT)*100/$UNIQUE_TOTAL" | bc)
+
 echo "Coverage: $RESULT%"
+echo "Unique coverage: $UNIQUE_RESULT%"
